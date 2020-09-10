4de2a33fa5e707820ff6869ddf83fb936f9b6098
Leandro Moscheni <leandro.moscheni@despegar.com>
2020-09-09 23:16:41 -0300

    Adding more env variables

--- src/resources/config/index.ts
+++ src/resources/config/index.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default (): any => ({
   port: process.env.PORT,
+  basePath: process.env.BASE_PATH,
   mongoUrl: process.env.MONGO_URL,
   mongoDbName: process.env.MONGO_DB_NAME,
   mongoUser: process.env.MONGO_USER,
